bdba17f26921162d52a9852a008ce29431268149
Rafael Missio <rtmissio@gmail.com>
2021-05-13 07:43:45 -0300

    Insert POST category

--- backend/src/main/java/br/com/mtisi/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/com/mtisi/dscatalog/services/CategoryService.java
@@ -33,4 +33,11 @@ public class CategoryService {
         return new CategoryDTO(category);
     }
 
+    public CategoryDTO insert(CategoryDTO dto){
+        Category entity = new Category();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CategoryDTO(entity);
+    }
+
 }
